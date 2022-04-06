602efdc0b28b83b7e659eed2a6d750e7e3b95806
Bruno Costa <bruno.costa@superlogica.com>
2022-04-06 20:18:57 -0300

    testando o cadastro de transação

--- tests/Feature/Services/TransactionServiceTest.php
+++ tests/Feature/Services/TransactionServiceTest.php
@@ -21,6 +21,34 @@ class TransactionServiceTest extends TestCase
 
         $this->assertDatabaseHas('transactions', [
             'amount' => 50,
+            'account_from_id' => $objAccount->id,
+            'pix_key_id' => $objPix->id,
+            'status' => 'pending'
+        ]);
+    }
+
+    public function test_generate_two_pix_and_transfer_between_theirs() {
+        $objAccount = Account::factory()->create();
+        $objPix = PixKey::factory()->create();
+
+        $objAccount1 = Account::factory()->create();
+        $objPix2 = PixKey::factory()->create();
+
+        $this->service()->newTransaction($objAccount, $objPix2, 50, 'teste');
+        $this->service()->newTransaction($objAccount1, $objPix, 30, 'teste');
+
+        $this->assertDatabaseHas('transactions', [
+            'amount' => 50,
+            'account_from_id' => $objAccount->id,
+            'pix_key_id' => $objPix2->id,
+            'status' => 'pending'
+        ]);
+
+        $this->assertDatabaseHas('transactions', [
+            'amount' => 30,
+            'account_from_id' => $objAccount1->id,
+            'pix_key_id' => $objPix->id,
+            'status' => 'pending'
         ]);
     }
 
