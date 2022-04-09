44e809000a5b5381a3c133fd8260befb3e13dd24
Bruno Costa <bruno.costa@superlogica.com>
2022-04-09 09:10:07 -0300

    resource

--- app/Http/Resources/TransactionResource.php
+++ app/Http/Resources/TransactionResource.php
@@ -14,6 +14,8 @@ class TransactionResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->uuid
+        ] + parent::toArray($request);
     }
 }
